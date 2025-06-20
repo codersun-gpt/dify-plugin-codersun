7eee4df65383784af89a489978ca973c27712470
liuyangyang <liuyangyang@tuhu.cn>
2025-06-20 16:14:56 +0800

    增加 file 类型输出

--- confluence_tools/tools/page_content.py
+++ confluence_tools/tools/page_content.py
@@ -1,6 +1,7 @@
 from collections.abc import Generator
 from typing import Any
 
+import io
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 from unit.confluence_session import ConfluenceSession
@@ -19,6 +20,7 @@ class PageContentTool(Tool):
         base_url = self.runtime.credentials.get('baseUrl')
         username = self.runtime.credentials.get('userName')
         password = self.runtime.credentials.get('password')
+        result_type = tool_parameters.get("result_type")
 
         if not all([base_url, username, password]):
             yield self.create_text_message(text="Missing required credentials")
@@ -36,9 +38,28 @@ class PageContentTool(Tool):
                 "title": result.get("title", "获取文档异常！")
             })
             return
-
-        yield self.create_json_message({
-            "success": True,
-            "title": result["title"],
-            "content": result["results"]
-        })
+        
+        fileTitle = result["title"]
+        content = result["results"]
+        
+        if result_type == 'file':
+            buffer = io.BytesIO()
+            buffer.write(content.encode('utf-8'))
+            buffer.seek(0)
+            yield self.create_blob_message(
+                blob=buffer.getvalue(),
+                meta={
+                    'mime_type': 'text/markdown',
+                    'filename': f"{fileTitle}",
+                    'original_filename': fileTitle,
+                    'save_as': f"{fileTitle}.md",
+                },
+            )
+            return
+        else:
+            # Return content as JSON
+            yield self.create_json_message({
+                "success": True,
+                "title": fileTitle,
+                "content": content
+            })
